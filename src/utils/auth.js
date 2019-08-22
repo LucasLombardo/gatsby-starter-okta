055d4a1b29a023e026bc123cdd5a83564bc5367f
Lucas <lucasl@bu.edu>
2019-08-21 20:48:38 -0400

    Add useAuth hook for getting user claims

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,5 @@
 import OktaSignIn from "@okta/okta-signin-widget"
-import { useEffect } from "react"
+import { useEffect, useLayoutEffect, useState } from "react"
 import { navigate } from "gatsby"
 
 const widget = new OktaSignIn({
@@ -7,6 +7,9 @@ const widget = new OktaSignIn({
   clientId: `${process.env.GATSBY_OKTA_CLIENT_ID}`,
   redirectUri: "http://localhost:8000/",
   logo: "//logo.clearbit.com/okta.com",
+  authParams: {
+    scopes: ["openid", "profile", "email"],
+  },
   features: {
     registration: true,
   },
@@ -20,7 +23,8 @@ export const useWidget = redirect => {
       },
       res => {
         if (res.status === "SUCCESS") {
-          console.log(res)
+          // add ID token to tokenManager
+          widget.authClient.tokenManager.add("user_id_token", res)
           navigate(redirect)
         }
       }
@@ -33,5 +37,33 @@ export const signOut = () => {
   widget.authClient
     .signOut()
     .catch(console.error)
-    .finally(() => navigate("/signin"))
+    .finally(() => {
+      // remove token from tokenManager
+      widget.authClient.tokenManager.remove("user_id_token")
+      navigate("/signin")
+    })
+}
+
+export const useAuth = () => {
+  const [loading, setLoading] = useState(true)
+  const [claims, setClaims] = useState(null)
+
+  const getClaims = async () => {
+    setLoading(true)
+    // get token from tokenManager
+    let token = await widget.authClient.tokenManager.get("user_id_token")
+    // verify token is valid
+    const isValid = token && (await widget.authClient.token.verify(token))
+    // set claims
+    if (isValid) {
+      setClaims(token.claims)
+    }
+    setLoading(false)
+  }
+
+  useLayoutEffect(() => {
+    getClaims()
+  }, [])
+
+  return { loading, claims }
 }
