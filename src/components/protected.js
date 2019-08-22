cf6a6f3be4e57d00bf0b81c2dbfb7e2e10ccf344
Lucas <lucasl@bu.edu>
2019-08-21 21:39:28 -0400

    Add protected component

---
+++ src/components/protected.js
@@ -0,0 +1,19 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { navigate } from "gatsby"
+import { useAuth } from "../utils/auth"
+
+const Protected = ({ children }) => {
+  const { loading, claims } = useAuth()
+  if (loading) return <div>Loading...</div>
+  if (!claims) {
+    navigate("/signin", { replace: true })
+  }
+  return <>{children}</>
+}
+
+Protected.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Protected
--- src/pages/protected-page.js
+++ src/pages/protected-page.js
@@ -2,11 +2,26 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Protected from "../components/protected"
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Protected Page" />
-    <h1>Protected Page</h1>
+    <Protected>
+      <h1>Protected Page</h1>
+      <p>This is a protected area of the site.</p>
+      <p>
+        If an unauthenticated user attempts to access this page, they will be
+        redirected to the signin page before anything inside of the
+        <code style={{ margin: "0.2rem" }}>Protected</code>
+        component is rendered.
+      </p>
+      <p>
+        While markup on this page will not be visible to regular users, it will
+        still be discoverable in your static assets. Be sure to keep any
+        sensitive data on your resource server.
+      </p>
+    </Protected>
   </Layout>
 )
 
